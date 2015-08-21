1a9c131310918f58dfe558dc4493375e40762e68
Tommy Chheng <tommy@tumblr.com>
2015-08-21 10:54:36 -0700

    Initial Commit

---
+++ TipCalcuator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalcuator
+//
+//  Created by Tommy Chheng on 8/21/15.
+//  Copyright (c) 2015 Tommy Chheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
